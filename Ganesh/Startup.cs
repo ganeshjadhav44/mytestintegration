c0f1a4f0a9145631c406958a89e8f458f83ec7de
Ganesh Jadhav <ganesh.jadhav@cloudtravellers.co.in>
2018-11-04 12:00:32 +0000

    Commited by Ganesh

---
+++ Ganesh/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Ganesh.Startup))]
+namespace Ganesh
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
